faf99bc26d12b933b9dc5259456d4baf193d4db8
Tega Ukavwe <justtega97@gmail.com>
2023-02-16 00:18:56 +0100

    different algorithm

--- app/blood_bank/views.py
+++ app/blood_bank/views.py
@@ -14,21 +14,10 @@ def ussd(request: HttpRequest) -> HttpResponse:
     service_code = request.POST.get("serviceCode", None)
     phone_number = request.POST.get("phoneNumber", None)
 
-    session = request.session
-    session.setdefault("name", None)
-    session.setdefault("mobile", None)
-    session.setdefault("address", None)
-    session.setdefault("quantity", None)
-    session.setdefault("blood_type", None)
-
-    if not session_id:
-        # When session ID is not provided, set it to None to generate a new session key
-        session_id = None
-
     response = HttpResponse(content_type=CONTENT_TYPE)
     if text == "":
         # This is the first request. Note how we start the response with CON
-        response.write("CON Welcome to USSD Blood Bank Service")
+        response.write("CON Welcome to USSD Blood Bank Service \n")
         response.write("What would you want to do? \n")
         response.write("1. Request blood \n")
         response.write("2. Donate blood \n")
@@ -43,23 +32,17 @@ def ussd(request: HttpRequest) -> HttpResponse:
         if word_count == 1:
             response.write("Enter your name:")
         elif word_count == 2:
-            session["name"] = request_history[LAST_INPUT]
             response.write("Enter your mobile:")
         elif word_count == 3:
-            session["mobile"] = request_history[LAST_INPUT]
             response.write("Enter your address:")
         elif word_count == 4:
-            session["address"] = request_history[LAST_INPUT]
-            session.save()
-            response.write(f"END {session['name']}, {session['mobile']}, {session['address']}, {session['blood_type']}")
+            response.write(f"END {request_history[1]}, {request_history[2]}, {request_history[3]}, {request_history[4]}")
 
     elif text == "2":
-        session["donating"] = 1
         response.write("CON Enter name")
     else:
         response.write("END Invalid choice")
 
-    session.save()  # Save the session data
     return response
 
     # Send the response back to the API
