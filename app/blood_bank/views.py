e02248f6dadd5c23a413339cb4546e5402b23511
Tega Ukavwe <justtega97@gmail.com>
2023-02-16 10:50:11 +0100

    Returned request_id from successful request

--- app/blood_bank/views.py
+++ app/blood_bank/views.py
@@ -2,44 +2,76 @@ from django.http import HttpResponse, HttpRequest
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
-LAST_INPUT = -1
-CONTENT_TYPE = "text/plain"
+from blood_bank.constants import *
+from blood_bank.utils import create_request_from_history
 
 
 @csrf_exempt
 @require_POST
 def ussd(request: HttpRequest) -> HttpResponse:
-    print(request.POST)
     text = request.POST.get("text", "default")
     session_id = request.POST.get("sessionId", None)
     service_code = request.POST.get("serviceCode", None)
-    phone_number = request.POST.get("phoneNumber", None)
+    requesting_number = request.POST.get("phoneNumber", None)
 
     response = HttpResponse(content_type=CONTENT_TYPE)
+
     if text == "":
         # This is the first request. Note how we start the response with CON
         response.write("CON Welcome to USSD Blood Bank Service \n")
-        response.write("What would you want to do? \n")
         response.write("1. Request blood \n")
         response.write("2. Donate blood \n")
 
-    elif text.startswith("1"):
-        request_history = text.split("*")
+    elif text[0] in ["1", "2"]:
+        request_history = [request_input for request_input in text.split("*") if request_input]
         word_count = len(request_history)
-        print(request_history)
 
-        if word_count < 4:
-            response.write("CON Blood Request: \n")
-
-        if word_count == 1:
-            response.write("Enter your name:")
-        elif word_count == 2:
-            response.write("Enter your mobile:")
-        elif word_count == 3:
-            response.write("Enter your address:")
-        elif word_count == 4:
-            response.write(f"END name={request_history[1]}\n mobile={request_history[2]}\n address={request_history[3]}")
-
+        if word_count == NAME_INDEX:
+            response.write("CON Enter name: \n")
+        elif word_count == MOBILE_INDEX:
+            response.write("CON Enter mobile: \n")
+        elif word_count == ADDRESS_INDEX:
+            response.write("CON Enter address: \n")
+        elif word_count == BLOOD_TYPE_INDEX:
+            response.write("CON Select blood type: \n")
+            for blood_type_index, blood_type in BLOOD_TYPES_MAP.items():
+                response.write(
+                    f"{blood_type_index}. {blood_type[HUMAN_READABLE_BLOOD_TYPE]} \n"
+                )
+        elif word_count == QUANTITY_INDEX:
+            response.write("CON Enter quantity (in pints): \n")
+        elif word_count == DATA_INTEGRITY:
+            request_type = (
+                "request" if request_history[REQUEST_TYPE] == "1" else "donation"
+            )
+            name = request_history[NAME_INDEX].strip().title()
+            mobile = request_history[MOBILE_INDEX]
+            address = request_history[ADDRESS_INDEX].strip().capitalize()
+            blood_type_as_int = int(request_history[BLOOD_TYPE_INDEX])
+            amount_in_pints = request_history[QUANTITY_INDEX]
+            blood_type = BLOOD_TYPES_MAP[blood_type_as_int][HUMAN_READABLE_BLOOD_TYPE]
+            response_data = (
+                f"CON BLOOD { request_type.upper() } \n\n"
+                f"Name: {name} \n"
+                f"Mobile: {mobile} \n"
+                f"Address: {address} \n"
+                f"Blood type: {blood_type} \n"
+                f"Quantity (in pints): {amount_in_pints} \n\n"
+                "Is the above data correct?\n\n"
+                "1. Yes \n"
+                "2. No"
+            )
+            response.write(response_data)
+        elif word_count == END_OF_REQUEST_INDEX:
+            proceed = request_history[CONFIRMED_INTEGRITY] == "1"
+            if proceed:
+                blood_request = create_request_from_history(request_history, requesting_number)
+                response.write("END Request created successful \n")
+                response.write(blood_request.request_id)
+            else:
+                response.write("END Requested terminated.")
+        else:
+            response.write("Invalid requests")
     elif text == "2":
         response.write("CON Enter name")
     else:
