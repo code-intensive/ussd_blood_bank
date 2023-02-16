6df1748d1283c24ce09723c9e76153c4c6bbbdc0
Tega Ukavwe <justtega97@gmail.com>
2023-02-16 10:48:37 +0100

    added constants to blood_bank app

---
+++ app/blood_bank/constants.py
@@ -0,0 +1,41 @@
+__all__ = (
+    "NAME_INDEX",
+    "CONTENT_TYPE",
+    "MOBILE_INDEX",
+    "REQUEST_TYPE",
+    "ADDRESS_INDEX",
+    "QUANTITY_INDEX",
+    "DATA_INTEGRITY",
+    "BLOOD_TYPES_MAP",
+    "BLOOD_TYPE_INDEX",
+    "LAST_INPUT_INDEX",
+    "ACTUAL_BLOOD_TYPE",
+    "CONFIRMED_INTEGRITY",
+    "END_OF_REQUEST_INDEX",
+    "HUMAN_READABLE_BLOOD_TYPE",
+)
+
+NAME_INDEX = 1
+REQUEST_TYPE = 0
+MOBILE_INDEX = 2
+ADDRESS_INDEX = 3
+QUANTITY_INDEX = 5
+DATA_INTEGRITY = 6
+BLOOD_TYPE_INDEX = 4
+LAST_INPUT_INDEX = -1
+ACTUAL_BLOOD_TYPE = 0
+CONFIRMED_INTEGRITY = 6
+END_OF_REQUEST_INDEX = 7
+CONTENT_TYPE = "text/plain"
+HUMAN_READABLE_BLOOD_TYPE = 1
+
+BLOOD_TYPES_MAP = {
+    1: ("o_positive", "O Positive"),
+    2: ("o_negative", "O Negative"),
+    3: ("a_positive", "A Positive"),
+    4: ("a_negative", "A Negative"),
+    5: ("b_positive", "B Positive"),
+    6: ("b_negative", "B Negative"),
+    7: ("ab_positive", "AB Positive"),
+    8: ("ab_negative", "AB Negative"),
+}
