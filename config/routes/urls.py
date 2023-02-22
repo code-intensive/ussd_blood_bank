99f6c5fd8f914646f0f55f9708ba8e40b0da9f5d
Tega Ukavwe <justtega97@gmail.com>
2023-02-22 07:50:52 +0100

    made the admin urls the root url

--- config/routes/urls.py
+++ config/routes/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("", admin.site.urls),
     path("blood-bank/", include("blood_bank.urls")),
 ]
